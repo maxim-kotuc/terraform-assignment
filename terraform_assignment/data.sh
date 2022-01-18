ec7bdf8e5b70d6f59f396fda29f4f8f210ad3882
Max <maxim.kotuc@gmail.com>
2022-01-18 23:09:09 +0100

    updated data

--- terraform_assignment/data.sh
+++ terraform_assignment/data.sh
@@ -3,6 +3,5 @@ sudo yum update -y
 sudo amazon-linux-extras install docker
 sudo service docker start
 sudo systemctl enable docker
-sudo usermod -a -G docker ec2-user
 sudo docker pull nginx
 sudo docker run -d -p 80:80 nginx
